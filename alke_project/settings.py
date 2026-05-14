993e18f80d36b9df8cacb0f97c9425c862ced44f
mmmaxii <mvalderrav@uc.cl>
2026-05-13 21:52:12 -0400

    fix production settings

--- alke_project/settings.py
+++ alke_project/settings.py
@@ -25,16 +25,11 @@ SECRET_KEY = 'django-insecure-ws_-(n%jebb80td$4!*e$$nv1+n38rq2i3rdpi#2n_!-4ow-l7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "alkewallet.up.railway.app",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://alke-wallet-django-production.up.railway.app"
 ]
-
 # Application definition
 
 INSTALLED_APPS = [
